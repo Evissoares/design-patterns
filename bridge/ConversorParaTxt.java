a4392e00d9e1db3b2e96210df0584d6bc4a99eb8
everton soares <everton.soares@live.com>
2022-03-27 20:09:20 -0300

    removido anotação desnecesária

--- bridge/ConversorParaTxt.java
+++ bridge/ConversorParaTxt.java
@@ -2,7 +2,6 @@ package bridge;
 
 public class ConversorParaTxt implements ConversorDocumento{
 	
-	@Override
 	public void converter(Documento doc) {
 		System.out.println(doc.getClass().getSimpleName() + " convertido para TXT");
 	}
